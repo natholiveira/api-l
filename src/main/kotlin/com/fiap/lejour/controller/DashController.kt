ab203800a29ffd7f40a42ffc445fa595f54ff50d
natholiveira <vo.nathalia@email.com>
2020-11-18 01:24:15 -0300

    cross origin

--- src/main/kotlin/com/fiap/lejour/controller/DashController.kt
+++ src/main/kotlin/com/fiap/lejour/controller/DashController.kt
@@ -15,7 +15,7 @@ class DashController(
 ) {
 
     @GetMapping(("/dash"))
-    @CrossOrigin(origins = arrayOf("http://localhost:8080"))
+    @CrossOrigin(origins = arrayOf("http://localhost:3000"))
     private fun getFaixa(@PathParam("mes") mes: MesEnum?): DashDTO {
 
         return DashDTO(
